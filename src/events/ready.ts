f0852383f46ea674c6ebb37c254d07256bd2289a
Nguyễn Hữu Hưng <360anime.suki@gmail.com>
2023-05-25 13:24:18 +0700

    Add guild logs

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -6,6 +6,14 @@ export default {
     name: Events.ClientReady,
     once: true,
     execute(client: Client<true>) {
+        const guilds = client.guilds.cache.map((guild) => guild.id);
+
+        // console.log(guilds);
+
+        for (const guild of guilds) {
+            const g = client.guilds.cache.get(guild);
+            console.log("Guilds", g.name);
+        }
         console.log(`Ready! Logged in as ${client.user.tag}`);
         client.user.setPresence({
             activities: [
