5dac1ced052ba2e69723109fc87347f0aca64088
Nguyễn Hữu Hưng <360anime.suki@gmail.com>
2023-06-27 13:59:31 +0700

    Update chat

--- src/commands/slash/chat.ts
+++ src/commands/slash/chat.ts
@@ -21,24 +21,31 @@ export default {
         ),
     async execute(interaction: ChatInputCommandInteraction) {
         interaction.deferReply();
-
         const content = interaction.options.getString("content");
 
-        const simsimi = await axios({
-            method: "GET",
-            baseURL: `https://api.simsimi.net/v2/?text=${encodeURI(
-                content
-            )}&lc=vn&key=${KEY_CHAT}`,
-            headers: { "Accept-Encoding": "gzip,deflate,compress" },
-        });
-
-        if (simsimi && simsimi.data?.success) {
-            return interaction.editReply({
-                content: `${bold(`Q: ${content}`)}\n${italic(
-                    `A: ${simsimi.data?.success}`
-                )}`,
+        try {
+            const simsimi = await axios({
+                method: "GET",
+                baseURL: `https://api.simsimi.net/v2/?text=${encodeURI(
+                    content
+                )}&lc=vn&key=${KEY_CHAT}`,
+                headers: { "Accept-Encoding": "gzip,deflate,compress" },
             });
-        } else {
+
+            if (simsimi && simsimi.data?.success) {
+                return interaction.editReply({
+                    content: `${bold(`Q: ${content}`)}\n${italic(
+                        `A: ${simsimi.data?.success}`
+                    )}`,
+                });
+            } else {
+                const embed = new EmbedBuilder()
+                    .setColor("Random")
+                    .setTimestamp();
+                embed.setTitle(`${content} not found the answer`);
+                return interaction.editReply({ embeds: [embed] });
+            }
+        } catch (error) {
             const embed = new EmbedBuilder().setColor("Random").setTimestamp();
             embed.setTitle(`${content} not found the answer`);
             return interaction.editReply({ embeds: [embed] });
