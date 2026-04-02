6a5f099ecdbbc7a4f14375b1a1d0ea3d6e7994a4
ds112 <16520479@gm.uit.edu.vn>
2026-04-02 15:23:17 +0700

    feat(voice): add lock, unlock, hide button handlers

---
+++ src/buttons/voiceLock.button.ts
@@ -0,0 +1,28 @@
+import { ButtonInteraction, MessageFlags, VoiceChannel } from "discord.js";
+import { BUTTON_ID } from "../util/config/button";
+import redis from "../connector/redis";
+import { validateOwner, checkCooldown, setCooldown, updatePanel } from "../util/voice/helpers";
+
+const TTL_12H = 60 * 60 * 12;
+
+export default {
+    id: BUTTON_ID.VOICE_LOCK,
+    async execute(interaction: ButtonInteraction) {
+        const voiceChannel = await validateOwner(interaction);
+        if (!voiceChannel) return;
+
+        const cdKey = `cd:lock:${voiceChannel.id}`;
+        if (!(await checkCooldown(interaction, cdKey))) return;
+
+        const everyone = interaction.guild!.roles.everyone;
+        await voiceChannel.permissionOverwrites.edit(everyone, {
+            Connect: false,
+            ViewChannel: true,
+        });
+
+        await redis.setJson(`state:${voiceChannel.id}`, "locked", TTL_12H);
+        await setCooldown(cdKey, 5);
+        await updatePanel(voiceChannel);
+        await interaction.reply({ content: "Channel locked 🔒", flags: MessageFlags.Ephemeral });
+    },
+};
